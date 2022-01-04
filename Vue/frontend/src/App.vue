7e231f2f2a8fdac17a8a91dfd2c0ac8ac2f82a8c
Tiago Kochem <57450432+Tiagokochem@users.noreply.github.com>
2022-01-04 20:22:48 -0300

    exerc

--- Vue/frontend/src/App.vue
+++ Vue/frontend/src/App.vue
@@ -1,21 +1,55 @@
 <template>
-  <h1>{{person.name}}</h1>
+<p v-for="todo in doneTodos" :key="todo.text">
+  {{ todo.text }}
+</p>
+
+<button @click="checkAllTodos">Finalizar</button>
+
+ <button @click="increment">Incrementar</button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-interface Person{
-  name: string
-  age: number
+interface Todo{
+  text: string
+  done: boolean
 }
 
 export default defineComponent({
     data: () {
       return {
-        person: {} as Person
+       todos: [] as Todo[]
+      }
+    },
+      computded: {
+    doneTodos(): Todo[] {
+      return this.todos.filter((todo) => todo.done)
+    }
+  },
+    watch: {
+      todos(newValue: Todo[]){
+        const isFinished = !newValue.some(({done})=> !done)
+     
+      if(isFinished){
+        alert('Boaaaaa!!!')
       }
     }
+},
+    created(){
+      this.todos = [
+  { text: 'Estudar Typescript', done: true },
+  { text: 'Lavar os pratos', done: false },
+  { text: 'Aprender Nuxt.js', done: true }
+    ]
+  },
+
+  method: {
+    checkAllTodos() {
+      this.todos = this.todos.map(({text}) => {
+        return {text, done: true}
+      })
+  }
   }
 })
 </script>
