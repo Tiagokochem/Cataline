203b78d419a501545618b1208872aeec7da110c0
Tiago Kochem <57450432+Tiagokochem@users.noreply.github.com>
2022-01-04 21:44:28 -0300

    v

---
+++ Vue/frontend/src/App4.vue
@@ -0,0 +1,22 @@
+<template>
+  <p> {{company}} é legal</p>
+  <p v-text="{{company}}"></p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup () {
+        
+
+        return {
+            company: 'Cataline'
+        }
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue/frontend/src/App5.vue
@@ -0,0 +1,21 @@
+<template>
+  <p v>Acesse: <span v-html="link"/></span></p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup () {
+        
+
+        return {
+            link: '<a href="http:tiagoconsultor.com">Tiago</a>'
+        }
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
