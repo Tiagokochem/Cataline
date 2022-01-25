bb124cc0c477241dbf6c8c093bb1ec4e2cfebba6
Tiago Kochem <57450432+Tiagokochem@users.noreply.github.com>
2022-01-25 06:30:20 -0300

    mysql

--- Adonis/backend/env.ts
+++ Adonis/backend/env.ts
@@ -18,7 +18,7 @@ export default Env.rules({
   HOST: Env.schema.string({ format: 'host' }),
   PORT: Env.schema.number(),
   APP_KEY: Env.schema.string(),
-  DB_CONECTION=mysql: Env.schema.string(0,
+  DB_CONECTION=mysql: Env.schema.string(),
   APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
