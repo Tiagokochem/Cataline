4281c0935924e0d21a07a7156f1f0159d1910a39
tkochem <tiagok989@gmail.com>
2022-02-08 06:43:56 -0300

    fb

---
+++ Facebook-api/facebookapi/app/Controllers/Http/Users/Register.ts
@@ -0,0 +1,43 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import {StoreValidator} from 'App/Validators/User/Register'
+import { User, UserKey, UserKey } from 'App/Models'
+import faker from 'faker'
+import Mail from '@ioc:Adonis/Addons/Mail'
+
+export default class UserRegisterController {
+
+    public async store({request}: HttpContextContract ) {
+        const { email, redirectUrl} = await request.validate(StoreValidator)
+        const user = await User.create({ email })
+
+        await user.save()
+
+        user.related('keys').create({ key: faker.datatype.uuid() +
+             new Date().getTime() })
+        
+        const link = `${redirectUrl.replace(/\/$/, '')}/${key}`
+
+    
+        await Mail.send((message) => {
+            message.to(email)
+            message.from('contato@facebook.com', 'Facebook')
+            message.subject('Criação de conta')
+            message.htmlView('emails/register', { link })
+        })
+
+    }
+
+
+    public async show({params}: HttpContextContract ) {
+        const UserKey = await UserKey.findByOrFail('key', params.key)
+        const user = await UserKey.related('user').query().firstOrFail()
+    
+        return user
+    }
+
+
+    public async update({}: HttpContextContract ) {}
+
+
+
+}
--- Facebook-api/facebookapi/app/Validators/Auth/index.ts
+++ Facebook-api/facebookapi/app/Validators/Auth/index.ts
@@ -1,2 +1,2 @@
-export { default as StoreValidator } from './StoreValidator'
+export { default as StoreValidator } from '../User/Register/StoreValidator'
 
